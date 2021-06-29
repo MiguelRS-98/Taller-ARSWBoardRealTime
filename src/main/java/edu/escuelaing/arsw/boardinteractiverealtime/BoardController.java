3994ef2bda0cd5df1b47eac88044c96904584392
Miguel Rodríguez <miguel.rodriguez-s@mail.escuelaing.edu.co>
2021-06-29 12:26:50 -0500

    Board v1

---
+++ src/main/java/edu/escuelaing/arsw/boardinteractiverealtime/BoardController.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.boardinteractiverealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardController 
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BoardController.class,args);
+    }
+}
